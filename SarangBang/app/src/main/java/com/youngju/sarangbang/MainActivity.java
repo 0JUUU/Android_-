f50166134a0b3425cfc51248f51433fd637aa06b
YOUNGJU <clleo97@naver.com>
2020-05-04 20:53:30 +0900

    Day1

---
+++ SarangBang/app/src/main/java/com/youngju/sarangbang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.youngju.sarangbang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post);
+    }
+}
